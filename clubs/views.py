44de8b296410557c6d4f4121f9498c0aa0b87349
Kevin Liao <me@liaokev.in>
2016-02-26 15:53:16 -0500

    Modified views and templates.

--- clubs/views.py
+++ clubs/views.py
@@ -5,9 +5,11 @@ from .models import Club
 
 def hello_world(request):
     clubs_list = Club.objects.order_by('name')
+    clubs_size = len(clubs_list)
     output = ', '.join([c.name for c in clubs_list])
     template = loader.get_template('clubs/index.html')
     context = {
-        'clubs_list': clubs_list
+        'clubs_list': clubs_list,
+        'clubs_size': clubs_size
     }
     return HttpResponse(template.render(context, request))
