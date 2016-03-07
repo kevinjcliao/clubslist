9e2a02f61dbd8270104e4e79492a7a1ec2f9df9a
Kevin Liao <me@liaokev.in>
2016-03-07 15:49:00 -0800

    Add nested category club list.

--- clubs/views.py
+++ clubs/views.py
@@ -4,12 +4,25 @@ from django.template import loader
 from .models import Club
 
 def hello_world(request):
-    clubs_list = Club.objects.order_by('name')
-    clubs_size = len(clubs_list)
-    output = ', '.join([c.name for c in clubs_list])
+    clubs_categories = Club.CATEGORY_CHOICES
+    categorized_club_list = []
+    clubs_size = 0
+    there_are_clubs = False
+    for x in range (0, len(clubs_categories)):
+        print "hello world"
+        category_id = clubs_categories[x][0]
+        clubs_in_category = Club.objects.all().filter(category=category_id).order_by('name')
+        categorized_club_list.append([clubs_categories[x][1], clubs_in_category])
+        clubs_size += len(clubs_in_category)
+
+    print categorized_club_list
+
+    there_are_clubs = clubs_size>0
+
     template = loader.get_template('clubs/index.html')
     context = {
-        'clubs_list': clubs_list,
-        'clubs_size': clubs_size
+        'clubs_list': categorized_club_list,
+        'clubs_size': clubs_size,
+        'there_are_clubs': there_are_clubs
     }
     return HttpResponse(template.render(context, request))
